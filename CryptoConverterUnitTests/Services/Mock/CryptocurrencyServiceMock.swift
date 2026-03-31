307047a48fa4be2b2acb0740dd0924d1ef1befe1
dimitris23bp <dimitris23bp@protonmail.com>
2026-03-31 22:26:34 +0200

    Fix & add tests for CryptocurrencyServiceUnitTests

--- CryptoConverterUnitTests/Services/Mock/CryptocurrencyServiceMock.swift
+++ CryptoConverterUnitTests/Services/Mock/CryptocurrencyServiceMock.swift
@@ -1,22 +1,22 @@
-//import Foundation
-//@testable import CryptoConverter
-//
-//@MainActor
-//final class CryptocurrencyServiceMock: CryptocurrencyServiceProtocol {
-//    var ensureInitialDataCalled = false
-//    var ensureInitialDataMinCount = 0
-//    var shouldThrowError = false
-//
-//    func ensureInitialDataIfNeeded(minCount: Int) async throws {
-//        ensureInitialDataCalled = true
-//        ensureInitialDataMinCount = minCount
-//        if shouldThrowError {
-//            throw NSError(domain: "MockError", code: 1, userInfo: nil)
-//        }
-//    }
-//    
-//    var mockLastUpdateString: String = "NaN"
-//    func getLastUpdate() async -> String {
-//        return mockLastUpdateString
-//    }
-//}
+import Foundation
+@testable import CryptoConverter
+
+@MainActor
+final class CryptocurrencyServiceMock: CryptocurrencyServiceProtocol {
+    var ensureInitialDataCalled = false
+    var ensureInitialDataMinCount = 0
+    var shouldThrowError = false
+
+    func ensureInitialDataIfNeeded(minCount: Int) async throws {
+        ensureInitialDataCalled = true
+        ensureInitialDataMinCount = minCount
+        if shouldThrowError {
+            throw NSError(domain: "MockError", code: 1, userInfo: nil)
+        }
+    }
+    
+    var mockLastUpdateString: String = "NaN"
+    func getLastUpdate() async -> String {
+        return mockLastUpdateString
+    }
+}
